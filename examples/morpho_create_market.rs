3433b722af0e3505d3ff7dc8e56351cbc680810b
Dario <dario@infinitefieldtrading.com>
2025-12-26 13:01:07 +0100

    examples: morpho create market event

---
+++ examples/morpho_create_market.rs
@@ -0,0 +1,127 @@
+use alloy::{
+    primitives::FixedBytes, providers::Provider, rpc::types::Filter, sol, sol_types::SolEvent,
+};
+use clap::Parser;
+use hypersdk::{
+    Address, U256,
+    hyperevm::{self, DynProvider, IERC20},
+};
+use tokio::sync::mpsc::unbounded_channel;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The address of the morpho vault.
+    #[arg(
+        short,
+        long,
+        default_value = "0x68e37de8d93d3496ae143f2e900490f6280c57cd"
+    )]
+    contract_address: Address,
+    /// RPC url
+    #[arg(short, long, default_value = "http://127.0.0.1:8545")]
+    rpc_url: String,
+}
+
+sol! {
+    contract Morpho {
+        type Id is bytes32;
+
+        struct MarketParams {
+            address loanToken;
+            address collateralToken;
+            address oracle;
+            address irm;
+            uint256 lltv;
+        }
+
+        event CreateMarket(Id indexed id, MarketParams marketParams);
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let _ = simple_logger::init_with_level(log::Level::Debug);
+    let args = Cli::parse();
+
+    println!("Connecting to RPC endpoint: {}", args.rpc_url);
+
+    let provider = DynProvider::new(hyperevm::mainnet_with_url(&args.rpc_url).await?);
+    let current_block = provider.get_block_number().await?;
+
+    #[derive(PartialEq, Eq, PartialOrd, Ord)]
+    struct Market {
+        id: FixedBytes<32>,
+        collateral_token: String,
+        loan_token: String,
+        irm: Address,
+        oracle: Address,
+        lltv: U256,
+    }
+
+    let (tx, mut rx) = unbounded_channel();
+    for from_block in (0..current_block).step_by(100_000) {
+        let provider = provider.clone();
+        let tx = tx.clone();
+
+        let filter = Filter::new()
+            .address(args.contract_address)
+            .event_signature(Morpho::CreateMarket::SIGNATURE_HASH)
+            .from_block(from_block)
+            .to_block(from_block + 100_000);
+
+        tokio::spawn(async move {
+            let logs = provider.get_logs(&filter).await?;
+            for log in logs {
+                let Some(topic0) = log.topic0() else {
+                    continue;
+                };
+
+                if topic0 == &Morpho::CreateMarket::SIGNATURE_HASH {
+                    if let Ok(market) = Morpho::CreateMarket::decode_log_data(&log.inner) {
+                        let collateral =
+                            IERC20::new(market.marketParams.collateralToken, provider.clone());
+                        let loan = IERC20::new(market.marketParams.loanToken, provider.clone());
+                        let (collateral, loan) = provider
+                            .multicall()
+                            .add(collateral.symbol())
+                            .add(loan.symbol())
+                            .aggregate()
+                            .await?;
+                        let _ = tx.send(Market {
+                            id: market.id,
+                            collateral_token: collateral,
+                            loan_token: loan,
+                            irm: market.marketParams.irm,
+                            oracle: market.marketParams.oracle,
+                            lltv: market.marketParams.lltv,
+                        });
+                    }
+                }
+            }
+
+            Ok::<_, anyhow::Error>(())
+        });
+    }
+
+    drop(tx);
+
+    let mut markets = vec![];
+    while let Some(create_market) = rx.recv().await {
+        markets.push(create_market);
+    }
+
+    markets.sort();
+
+    for market in markets {
+        println!("------------");
+        println!("market: {}", market.id);
+        println!("collateral: {}", market.collateral_token);
+        println!("loan token: {}", market.loan_token);
+        println!("oracle: {}", market.oracle);
+        println!("irm: {}", market.irm);
+        println!("LLTV: {}", market.lltv);
+    }
+
+    Ok(())
+}
